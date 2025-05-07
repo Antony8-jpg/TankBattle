44fff582ec61383dd4e573a19cf52d5c05137ac2
DieterM34 <dietermertens1705@gmail.com>
2025-05-07 20:10:21 +0200

    Update main.py

--- project/main.py
+++ project/main.py
@@ -4,16 +4,14 @@ import pygame
 from Variabels import *
 from ScreenObjects import *
 from Powerups import *
+from MovingObjects import *
 
 import pygame
 import math
 import random
 from queue import PriorityQueue
 
-
-
 # TO DO:
-# startscreen met knoppen
 # meerdere bots
 # in meerdere files zetten
 
@@ -21,60 +19,6 @@ pygame.init()
 clock = pygame.time.Clock()
 current_time = pygame.time.get_ticks()        
 last_print_time = 0
-
-
-class MovingObject(Object):
-    def __init__(self,pos):
-        super().__init__(pos)
-        
-    def manage(self, bullet_list , yourobject, other_object):
-        for bullet in bullet_list[:]: #kopie van de lijst gebruiken om veilig bullets te verwijderen
-            if not bullet.launch(): #als launch False geeft
-                bullet_list.remove(bullet)
-            else:
-                rotated_bullet = pygame.transform.rotate(bullet.image, bullet.angle)  #kogel wordt geroteerd
-                screen.blit(rotated_bullet, bullet.rect.topleft)
-                
-                for object in list_of_objects:
-                    if bullet.collision(object):
-                        if isinstance(object, Wall) and isinstance(bullet, SpecialBullet):
-                            list_of_objects.remove(object)  # SpecialBullet detroyed walls
-                        elif isinstance(object, Bush):
-                            list_of_objects.remove(object)
-                        bullet_list.remove(bullet)
-                        break  # voorkomt dubbele verwijdering van een bullet bij 2 collisions
-                
-                if bullet not in bullet_list:  #Als de bullet al verwijderd is, niet verder checken
-                    continue 
-                        
-                if bullet.collision(other_object):
-                    bullet_list.remove(bullet)
-                    if hasattr(other_object, 'has_shield') and other_object.has_shield:
-                        other_object.has_shield = False  # Shield verliest bescherming
-                        
-                    else:
-                        damage = 2 if isinstance(bullet, SpecialBullet) else 1
-                        other_object.health -= damage
-                        hit_sound.play()
-                    break
-
-    def blit_rotated_image(surf, image, center_pos, angle, true_rect_size):
-        # roteer de image
-        rotated_image = pygame.transform.rotate(image, angle)
-        rotated_rect = rotated_image.get_rect(center=center_pos)
-
-        # teken de geroteerde afbeelding
-        surf.blit(rotated_image, rotated_rect.topleft)
-
-        # true_rect voor de rectangle die niet roteert, deze wordt gebruikt voor collisions ...
-        true_rect = pygame.Rect(
-            center_pos[0] - true_rect_size[0] // 2,
-            center_pos[1] - true_rect_size[1] // 2,
-            true_rect_size[0],
-            true_rect_size[1]
-            )
-
-        return true_rect
 
 class Player(MovingObject):
     def __init__(self,pos,direction,player_speed,rotation_speed,angle,playerImg):
@@ -168,34 +112,6 @@ class Player(MovingObject):
         self.has_shield = False
         self.speed_boost_active = False
         self.speed_boost_start_time = 0
-
-class Bullet(MovingObject):
-    def __init__(self, pos, direction, speed, angle, bulletIMG):
-        super().__init__(pos)
-        self.direction = direction
-        self.speed = speed
-        self.angle = angle
-        self.image = bulletIMG
-        self.rect = self.image.get_rect(center = self.pos)
-            
-    
-    def launch(self):
-        self.pos += self.speed * self.direction 
-        self.rect.center = self.pos
-        rotated_bullet = pygame.transform.rotate(self.image, -self.angle)
-        self.rect = rotated_bullet.get_rect(center = self.pos)
-        
-        
-        if self.pos.x < 0 or self.pos.x > screen_length or self.pos.y < 0 or self.pos.y > screen_height:
-           return False  # Geeft False om bullet te verwijderen uit de bullet lijst
-        return True  # Bullet blijft in de lijst
-        
-class SpecialBullet(Bullet):
-    def __init__(self, pos, direction, speed, angle, bulletIMG):
-        super().__init__(pos, direction, speed, angle, bulletIMG)
-        
-    def launch(self):
-        return super().launch()
 
 class Bot(MovingObject):
     def __init__(self, pos,image,speed,angle):
@@ -503,7 +419,7 @@ player = Player(player_pos, direction, player_speed, rotation_speed, angle,playe
 bot = Bot(bot_pos,bot_image,bot_speed,bot_angle)
 
 #walls and bushes generaten
-list_of_objects = [] #walls en bushes worden in deze lijst geplaatst
+"""list_of_objects = [] #walls en bushes worden in deze lijst geplaatst"""
 available_positions = Screen.available_positions()
 walls = GenerateObject(amount= wall_amount , object= Wall, image = wall_image)
 walls.generate()
